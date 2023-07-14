0f0ec8ed399690edb0630de660d7a3c34bffe2e1
Pedro Romero <108835699+pedrofaro1512@users.noreply.github.com>
2023-07-14 11:21:33 -0500

    Adición de ventana emergente para tweet

--- src/components/SendTweet/SendTweet.js
+++ src/components/SendTweet/SendTweet.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Fab } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import moment from "moment";
+import ModalContainer from "../ModalContainer";
+import FormSendTweet from "../FormSendTweet";
+import { TWEETS_STORAGE } from "../../Utils/constants";
 
 //Importación de estilos
 import "./SendTweet.scss";
 
 const SendTweet = () => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const openModal = () => {
+    setIsOpenModal(true);
+  };
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
+
+  const sendTweet = (event, formValue) => {
+    event.preventDefault();
+    const { name, tweet } = formValue;
+    let allTweetsArray = [];
+
+    if (!name || !tweet) {
+      console.log("WARNING: Todos los espacios son requeridos");
+    } else {
+      formValue.time = moment();
+      allTweetsArray.push(formValue);
+      localStorage.setItem(TWEETS_STORAGE, JSON.stringify(allTweetsArray));
+      console.log("Tweet enviado correctamente");
+      closeModal();
+    }
+    allTweetsArray = [];
+  };
+
   return (
     <div className="send-tweet">
-      <Fab className="send-tweet__open-modal" color="primary" aria-label="add">
+      <Fab
+        className="send-tweet__open-modal"
+        color="primary"
+        aria-label="add"
+        onClick={openModal}
+      >
         <AddIcon />
       </Fab>
+      <ModalContainer isOpenModal={isOpenModal} closeModal={closeModal}>
+        <FormSendTweet sendTweet={sendTweet} />
+      </ModalContainer>
     </div>
   );
 };
